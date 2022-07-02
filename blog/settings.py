ff2faed4a044845f666a815be8d2003e41a6e13d
Łukasz Żywański <lukasz.zywanski@sollers.eu>
2022-07-02 09:54:07 +0200

    hide secret key 2

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mvh5rf+zsc_p040xl_ahm3eh27!gj9z^awoj1ad$m1uo1%v(g)'
+SECRET_KEY = config("APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
